805951e7b0c75b3414a990c023672e3827317fb6
rodolforoc <rodolforoc97@gmail.com>
2020-12-24 10:52:12 -0300

    finish authentication

--- lib/screens/auth_screen.dart
+++ lib/screens/auth_screen.dart
@@ -1,4 +1,8 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:myChat/models/auth_data.dart';
 import 'package:myChat/widgets/auth_form.dart';
 
 class AuthScreen extends StatefulWidget {
@@ -7,11 +11,88 @@ class AuthScreen extends StatefulWidget {
 }
 
 class _AuthScreenState extends State<AuthScreen> {
+  final _auth = FirebaseAuth.instance;
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+
+  bool _isLoading = false;
+
+  Future<void> _handleSubmit(AuthData authData) async {
+    setState(() {
+      _isLoading = true;
+    });
+
+    AuthResult authResult;
+
+    try {
+      if (authData.isLogin) {
+        authResult = await _auth.signInWithEmailAndPassword(
+          email: authData.email.trim(),
+          password: authData.password,
+        );
+      } else {
+        authResult = await _auth.createUserWithEmailAndPassword(
+          email: authData.email.trim(),
+          password: authData.password,
+        );
+
+        final userData = {
+          'name': authData.name,
+          'email': authData.email,
+        };
+
+        await Firestore.instance
+            .collection('users')
+            .document(authResult.user.uid)
+            .setData(userData);
+      }
+    } on PlatformException catch (error) {
+      final msg =
+          error.message ?? 'Ocorreu um erro! Verifique suas credenciais';
+      _scaffoldKey.currentState.showSnackBar(
+        SnackBar(
+          content: Text(msg),
+          backgroundColor: Theme.of(context).errorColor,
+        ),
+      );
+    } catch (error) {
+      print(error);
+    } finally {
+      setState(() {
+        _isLoading = false;
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      key: _scaffoldKey,
       backgroundColor: Theme.of(context).primaryColor,
-      body: AuthForm(),
+      // body: AuthForm(_handleSubmit),
+      body: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Stack(
+              children: [
+                AuthForm(_handleSubmit),
+                if (_isLoading)
+                  Positioned.fill(
+                    child: Container(
+                      margin: EdgeInsets.all(20),
+                      decoration: BoxDecoration(
+                        color: Color.fromRGBO(0, 0, 0, 0.5),
+                      ),
+                      child: Center(
+                        child: CircularProgressIndicator(),
+                      ),
+                    ),
+                  )
+              ],
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
--- lib/widgets/auth_form.dart
+++ lib/widgets/auth_form.dart
@@ -1,11 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:myChat/models/auth_data.dart';
 
 class AuthForm extends StatefulWidget {
+  final void Function(AuthData authData) onSubmit;
+
+  AuthForm(this.onSubmit);
+
   @override
   _AuthFormState createState() => _AuthFormState();
 }
 
 class _AuthFormState extends State<AuthForm> {
+  final GlobalKey<FormState> _formKey = GlobalKey();
+  final AuthData _authData = AuthData();
+
+  void _submit() {
+    bool isValid = _formKey.currentState.validate();
+    FocusScope.of(context).unfocus();
+
+    if (isValid) {
+      widget.onSubmit(_authData);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Center(
@@ -15,33 +32,68 @@ class _AuthFormState extends State<AuthForm> {
           child: Padding(
             padding: EdgeInsets.all(16),
             child: Form(
+              key: _formKey,
               child: Column(
                 children: [
-                  TextFormField(
-                    decoration: InputDecoration(
-                      labelText: 'Nome',
+                  if (_authData.isSignup)
+                    TextFormField(
+                      key: ValueKey('name'),
+                      decoration: InputDecoration(
+                        labelText: 'Nome',
+                      ),
+                      onChanged: (value) => _authData.name = value,
+                      validator: (value) {
+                        if (value == null || value.trim().length < 4) {
+                          return 'Nome deve ter no mínimo 4 caracteres';
+                        }
+
+                        return null;
+                      },
                     ),
-                  ),
                   TextFormField(
+                    key: ValueKey('email'),
                     decoration: InputDecoration(
                       labelText: 'E-mail',
                     ),
+                    onChanged: (value) => _authData.email = value,
+                    validator: (value) {
+                      if (value == null || !value.contains('@')) {
+                        return 'Forneça um e-mail válido';
+                      }
+
+                      return null;
+                    },
                   ),
                   TextFormField(
+                    key: ValueKey('password'),
                     obscureText: true,
                     decoration: InputDecoration(
                       labelText: 'Senha',
                     ),
+                    onChanged: (value) => _authData.password = value,
+                    validator: (value) {
+                      if (value == null || value.trim().length < 7) {
+                        return 'Senha deve ter no mínimo 7 caracteres';
+                      }
+
+                      return null;
+                    },
                   ),
                   SizedBox(height: 12),
                   RaisedButton(
-                    child: Text('Entrar'),
-                    onPressed: () {},
+                    child: Text(_authData.isLogin ? 'Entrar' : 'Cadastrar'),
+                    onPressed: _submit,
                   ),
                   FlatButton(
                     textColor: Theme.of(context).primaryColor,
-                    child: Text('Criar uma nova conta?'),
-                    onPressed: () {},
+                    child: Text(_authData.isLogin
+                        ? 'Criar uma nova conta?'
+                        : 'Já possui uma conta?'),
+                    onPressed: () {
+                      setState(() {
+                        _authData.toggleMode();
+                      });
+                    },
                   )
                 ],
               ),
